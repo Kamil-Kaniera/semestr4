b7d9cebded6ac9f0348f2df7d57ace5f56a03954
Krzysztof <kr.purgat@gmail.com>
2024-03-19 16:19:20 +0100

    Krypto, Zadanie 1 - funkcje zwracające strony bloku, zamiany w GUI

--- Kryptografia/Triple_DES/src/main/java/view/MainViewController.java
+++ Kryptografia/Triple_DES/src/main/java/view/MainViewController.java
@@ -1,4 +1,44 @@
 package view;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
 public class MainViewController {
+    private Stage stage;
+    @FXML
+    private TextField firstKey;
+    @FXML
+    private TextField secondKey;
+    @FXML
+    private TextField thirdKey;
+
+
+    @FXML
+    private void initialize() {
+    }
+
+    @FXML
+    private void onKeyGeneratorButtonClick() {
+        firstKey.setText("0123456789ABCDEF");
+        secondKey.setText("1133557799BBDDFF");
+        thirdKey.setText("0022446688AACCEE");
+    }
+
+    @FXML
+    private void onLoadButtonClick() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open Resource File");
+        fileChooser.showOpenDialog(stage);
+    }
+
+    @FXML
+    private void onSaveButtonClick() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open Resource File");
+        fileChooser.showSaveDialog(stage);
+    }
+
+
 }
